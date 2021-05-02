68971476f0e0f82d38a3cc9e00255b8dd4931569
simonproudfoot <simonproudfoot1980@gmail.com>
2021-05-02 16:48:05 +0100

    heroku settings 2

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ var express = require('express');
 var expressWs = require('express-ws');
 var expressWs = expressWs(express());
 var app = expressWs.app;
-
 app.use(express.static('public'));
-
 var aWss = expressWs.getWss('/');
+// // homepage
 
-app.ws('/', function(ws, req) {
+app.get('/', (req, res) => {
+  console.error('express connection');
+  res.sendFile(path.join(__dirname, 'ws.html'));
+});
+
+app.ws('/totem', function(ws, req) {
   console.log('New totem connected');
-
   ws.onmessage = function(msg) {
     console.log(msg.data);
     aWss.clients.forEach(function (client) {
@@ -18,5 +21,4 @@ app.ws('/', function(ws, req) {
     });
   };
 });
-
 app.listen(3001);
