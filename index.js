3655731bf9495aef18fd8f19d0b6b0aa2531e26d
simonproudfoot <simonproudfoot1980@gmail.com>
2021-05-04 16:00:18 +0100

    working version

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var ip = require("ip");
+const ip = require('request-ip');
 var express = require('express');
 var expressWs = require('express-ws');
 var expressWs = expressWs(express());
@@ -8,18 +8,75 @@ var aWss = expressWs.getWss('/');
 // homepage
 const port = process.env.PORT || 3001
 
+const defaultMinute = 3
+const speed = 6000 // change this to speed up for testing: 60000 = 1 min
+var m = defaultMinute
+
+var timer = false
+
+function countdown() {
+  function run() {
+    timer = setInterval(() => {
+      if (m > -1) {
+        aWss.clients.forEach(function (client) {
+          client.send(m);
+        });
+        console.log(m--)
+      }
+    }, speed);
+  }
+
+  if (timer != false) {
+    clearInterval(timer)
+    run()
+  } else {
+    run()
+  }
+}
+
 app.get('/', (req, res) => {
   console.error('express connection');
   res.sendFile(path.join(__dirname, 'ws.html'));
 });
 
-app.ws('/totem', function(ws, req) {
+
+app.ws('/totem', (ws, req) => {
+
+  // restart timer when new totem connects
   console.log('New totem connected');
-  ws.onmessage = function(msg) {
-    console.log(msg.data);
-    aWss.clients.forEach(function (client) {
-      client.send(msg.data);
-    });
+  //console.log('Listening on:'+req.ip+':'+port)
+  m = defaultMinute
+
+  // LISTEN FOR MESSAGE
+  ws.onmessage = (msg) => {
+    console.log(msg.data)
+    // START 
+    if (msg.data == 'start') {
+      console.log('starting')
+      m = defaultMinute
+      countdown()
+      aWss.clients.forEach(function (client) {
+        client.send(defaultMinute);
+      });
+    }
+
+    // STOP
+    if (msg.data == 'stop') {
+      console.log('stopping')
+      counting = false
+      //  clearInterval(countdown)
+    }
+
+    // RESET
+    if (msg.data == 'resetTimer') {
+      counting = false
+      console.log('reseting')
+      m = defaultMinute
+      aWss.clients.forEach(function (client) {
+        client.send(defaultMinute);
+      });
+      counting = true
+    }
   };
 });
 
